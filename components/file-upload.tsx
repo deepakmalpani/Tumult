8757a17f08dc5b149d4e6f2a7918a1dc1b242c1a
Deepak Malpani <deepak.malpani@exxonmobil.com>
2024-06-19 10:37:21 +0530

    feat: Add lazy loading for images in FileUpload component
    The code changes in the FileUpload component add lazy loading for images. This improves the page load performance by loading images only when they are visible in the viewport. The lazy loading is implemented using the next/image component. This enhancement provides a better user experience by reducing the initial load time and optimizing the rendering of images.

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { X } from "lucide-react";
+import Image from "next/image";
+
 import { UploadDropzone } from "@/lib/upload-thing";
 
 import "@uploadthing/react/styles.css";
@@ -11,11 +14,27 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
-        onChange(res?.[0].fileUrl);
+        onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
         console.error(error);
