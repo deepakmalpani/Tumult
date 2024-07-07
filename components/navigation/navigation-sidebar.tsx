8404d4150578cdcb553943179b6be60f30bc2392
Deepak Malpani <deepak.malpani@exxonmobil.com>
2024-07-07 16:04:00 +0530

    feat: Add NavigationItem component
    This commit adds a new component called NavigationItem. The component represents an item in the navigation sidebar and includes functionality for navigating to specific servers. It enhances the application by providing a visual representation of server items and allowing users to easily switch between servers.

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import NavigationAction from "./navigation-action";
 import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import NavigationItem from "./navigation-item";
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -25,6 +27,17 @@ const NavigationSidebar = async () => {
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
       <Separator className="h-[2px] bg-zin-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-l w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
