09b38e00be3250b8a192c1394a82e131ebe84ea1
Deepak Malpani <deepak.malpani@exxonmobil.com>
2024-07-08 11:36:47 +0530

    feat: Add ServerIDLayout component
    This commit adds a new component called ServerIDLayout. The component represents the layout for a specific server and includes a sidebar and main content area. It checks if the user is authenticated and if they are a member of the server before rendering the layout. This component enhances the application by providing a structured layout for server-specific pages and improving code organization.

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,43 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const ServerIDLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    redirect("/");
+  }
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        Server Sidebar
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
+};
+
+export default ServerIDLayout;
