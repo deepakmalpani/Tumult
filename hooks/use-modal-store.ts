3e5eff36f772af7a6e8a352286182659e3483349
Deepak Malpani <deepak.malpani@exxonmobil.com>
2024-08-10 16:12:59 +0530

    feat: Add "deleteServer" modal type to use-modal-store.ts

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,10 +1,16 @@
-
 import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer";
+export type ModalType =
+  | "createServer"
+  | "invite"
+  | "editServer"
+  | "members"
+  | "createChannel"
+  | "leaveServer"
+  | "deleteServer";
 
-interface ModalData{
+interface ModalData {
   server?: Server;
 }
 
@@ -12,7 +18,7 @@ interface ModalStore {
   type: ModalType | null;
   data: ModalData;
   isOpen: boolean;
-  onOpen: (type: ModalType, data? : ModalData) => void;
+  onOpen: (type: ModalType, data?: ModalData) => void;
   onClose: () => void;
 }
 
