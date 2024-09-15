e36dc61b7fedbeb8ec356ea8cac07bf591362fd5
deepakmalpani <deepakmalpan0@gmail.com>
2024-09-15 19:19:14 +0530

    feat: Enhance ChatInput to reset form and refresh router after message submission

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -7,9 +7,10 @@ import axios from "axios";
 import qs from "query-string";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "../ui/input";
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
 import { EmojiPicker } from "../emoji-picker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -23,7 +24,8 @@ const formSchema = z.object({
 });
 
 const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
-  const {onOpen} = useModal();
+  const { onOpen } = useModal();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -41,6 +43,8 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
         query,
       });
       await axios.post(url, values);
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
@@ -58,7 +62,7 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() => onOpen("messageFile", {apiUrl, query})}
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px]
                                     bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 
                                     dark:hover:bg-zinc-300 transition rounded-full
@@ -78,7 +82,9 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                   />
                   <div className="absolute top-7 right-8">
                     <EmojiPicker
-                      onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value} ${emoji}`)
+                      }
                     />
                   </div>
                 </div>
